2da9905380b430c3bf4fd924bc95f3da4220d538
Tanramos123 <172038510+CristanRamos@users.noreply.github.com>
2025-09-10 18:10:41 +0800

    try

--- backend/server.js
+++ backend/server.js
@@ -180,9 +180,10 @@ const io = new Server(server, {
 
 // Middleware
 app.use(cors({
-  origin: "*",
+  origin: ["*", "https://login-signup-3470.onrender.com/api/forgot-password"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
 }));
 
 app.use(bodyParser.json());
