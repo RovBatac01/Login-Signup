971f8bf14ded4dff66cd9372d871fcbeeb8b0807
Rov <rovicbatacandolo@gmail.com>
2025-10-23 05:28:10 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -182,7 +182,7 @@ const authenticateAdminRoute = (req, res, next) => {
     }
 };
 
-const allowedOrigins = ["https://aquasensesolutions.onrender.com", "https://login-signup-production-e1ef.up.railway.app" ];
+const allowedOrigins = ["https://login-signup-production-e1ef.up.railway.app" ];
 
 // 2. Add all your middleware for parsing and security
 app.use(bodyParser.json());
